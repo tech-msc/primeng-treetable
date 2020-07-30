c17f85407e587dc47adf7fde35d27533e369b14b
tech-msc <23535116+tech-msc@users.noreply.github.com>
2020-07-29 23:58:05 -0300

    scaffold project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DataJsonService } from './home/home.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [DataJsonService],
+  // bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
